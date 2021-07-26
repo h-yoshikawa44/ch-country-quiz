4a5fcbf76757fb64f68ef5583cc897097a81eb12
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-07-26 17:42:12 +0900

    feat: クイズのカスタムフック仮作成(#5)

---
+++ src/models/Country.ts
@@ -0,0 +1,22 @@
+export type Countrys = Country[];
+
+export type Country = {
+  name: string;
+  topLevelDomain: string[];
+  alpha2Code: string;
+  alpha3Code: string;
+  callingCodes: string[];
+  capital: string;
+  altSpellings: string[];
+  region: string;
+  subregion: string;
+  population: number;
+  latlng: number[];
+  demonym: string;
+  area: number;
+  gini: number;
+  timezones: string[];
+  borders: string[];
+  nativeName: string;
+  numericCode: string;
+};
