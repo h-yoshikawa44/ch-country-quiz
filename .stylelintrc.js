36104261e9ab22241e6a98bbdcf4152a000f4dd3
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-01-07 14:08:05 +0900

    update・settings: Pre Commit設定、各種静的解析の設定更新(#6)

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,5 +5,11 @@ module.exports = {
     'stylelint-config-prettier',
   ],
   plugins: ['stylelint-order'],
+  overrides: [
+    {
+      files: ['**/*.{ts,tsx}'],
+      customSyntax: '@stylelint/postcss-css-in-js',
+    },
+  ],
   ignoreFiles: ['**/node_modules/**'],
 };
