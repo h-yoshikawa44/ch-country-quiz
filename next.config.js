c5a29a0fc6493a03915ea1ecfa50d53d07683071
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-10-15 18:18:45 +0900

    feat: emotionのCSS propをNext.js側の機能を通して使うようにした(#14)
    これによりBebelの設定が不要にできた。

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    emotion: true,
+  },
   images: {
     domains: ['flagcdn.com'],
   },
 };
+
+module.exports = nextConfig;
